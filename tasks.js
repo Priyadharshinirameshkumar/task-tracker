83c2568152c03416b411825fce2e7fc9e1ef7651
Priyadharshini R <priyadharshinirameshkumar19@gmail.com>
2026-06-30 13:16:58 +0530

    part 4: implement TypeScript core types

--- tasks.js
+++ tasks.js
@@ -1,3 +1,31 @@
-"use strict";
-console.log("Task Tracker");
+// ======================================
+// Task Interface
+// ======================================
+// ======================================
+// Tasks Array
+// ======================================
+export let tasks = [];
+// ======================================
+// Add Task
+// ======================================
+export function addTask(name, priority, dueDate) {
+    const task = {
+        id: Date.now(),
+        name,
+        priority,
+        dueDate,
+        done: false
+    };
+    tasks.push(task);
+    return task;
+}
+// ======================================
+// Toggle Done
+// ======================================
+export function toggleDone(id) {
+    const task = tasks.find(task => task.id === id);
+    if (task) {
+        task.done = !task.done;
+    }
+}
 //# sourceMappingURL=tasks.js.map
--- tasks.ts
+++ tasks.ts
@@ -1,1 +1,81 @@
-console.log("Task Tracker");
+// ======================================
+// Task Interface
+// ======================================
+
+export interface Task {
+
+    id: number;
+
+    name: string;
+
+    priority: "Low" | "Medium" | "High";
+
+    dueDate: string;
+
+    done: boolean;
+
+}
+
+// ======================================
+// Tasks Array
+// ======================================
+
+export let tasks: Task[] = [];
+
+// ======================================
+// Add Task
+// ======================================
+
+export function addTask(
+
+    name: string,
+
+    priority: "Low" | "Medium" | "High",
+
+    dueDate: string
+
+): Task {
+
+    const task: Task = {
+
+        id: Date.now(),
+
+        name,
+
+        priority,
+
+        dueDate,
+
+        done: false
+
+    };
+
+    tasks.push(task);
+
+    return task;
+
+}
+
+// ======================================
+// Toggle Done
+// ======================================
+
+export function toggleDone(
+
+    id: number
+
+): void {
+
+    const task = tasks.find(
+
+        task => task.id === id
+
+    );
+
+    if (task) {
+
+        task.done = !task.done;
+
+    }
+
+}
